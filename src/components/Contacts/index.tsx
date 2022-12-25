57d705d51d22107d82b6f4befab3900037183f20
Vadim Tulbu <vt@insomniacdesign.com>
2022-12-25 19:04:48 +0200

    - Delete fragment from contacts component - Simplify raiting component - Create pages folder

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,21 +1,22 @@
-import { FC } from "react";
-import * as S from "./styled";
-import { StyledProps } from "./types";
+import { FC } from 'react';
+import * as S from './styled';
+import { StyledProps } from './types';
 import logo from '../../images/illustration-thank-you.svg';
 
-export const Contacts: FC<
-  React.PropsWithChildren<StyledProps>
-> = ({ value }) => {
+export const Contacts: FC<React.PropsWithChildren<StyledProps>> = ({
+  value,
+}) => {
   return (
-    <>
-      <S.TextWrapper>
-        <S.ImageBox>
-          <img src={logo} className="thanks-img" alt="logo" />
-        </S.ImageBox>
-        <span>{`You selected ${value} out of 5`}</span>
-        <span>Thank you!</span>
-        <span>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</span>
-      </S.TextWrapper>
-    </>
+    <S.TextWrapper>
+      <S.ImageBox>
+        <img src={logo} className='thanks-img' alt='logo' />
+      </S.ImageBox>
+      <span>{`You selected ${value} out of 5`}</span>
+      <span>Thank you!</span>
+      <span>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </span>
+    </S.TextWrapper>
   );
 };
--- src/components/Raiting/index.tsx
+++ src/components/Raiting/index.tsx
@@ -1,71 +1,50 @@
-import { FC, useEffect, useState } from "react";
-import { Contacts } from "../Contacts";
-import * as S from "./styled";
+import { FC, useState } from 'react';
+import * as S from './styled';
 import logo from '../../images/icon-star.svg';
 
 export const Rating: FC = () => {
-  const [rating, setRating] = useState(0);
-  const [isDisabled, setDisabled] = useState(false);
-  const [submit, setSubmit] = useState(rating);
+  const [rating, setRating] = useState<number | null>(null);
 
-
-  useEffect(() => {
-    if (rating) {
-      setDisabled(true);
-    }
-  }, [rating]);
-
-  const handleClick = () => {
-    setSubmit(currentValue => {
-      return currentValue + rating
-    });
-  };
-
-  console.log('rating', rating);
-  console.log('submit', submit);
   return (
     <S.HeroWrapper>
-      {submit === 0 ?
-        <>
-          <S.ImageBox>
-            <img src={logo} className="App-logo" alt="logo" />
-          </S.ImageBox>
-          <S.Text>
-            <p>How did we do?</p>
-            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-          </S.Text>
-          <S.RatingButtons>
-            {[...Array(5)].map((star, index) => {
-              star = index += 1;
+      {/*Separate component*/}
+      <S.ImageBox>
+        <img src={logo} className='App-logo' alt='logo' />
+      </S.ImageBox>
 
-              console.log('tester', rating);
-              return (
-                <S.Button
-                  disabled={isDisabled && rating !== index && rating !== 0}
-                  type="button"
-                  key={index}
-                  className={index === rating ? "rating-button on" : "rating-button off"}
-                  onClick={() => setRating(index)}
-                  onDoubleClick={() => {
-                    setRating(0);
-                  }}
-                >
-                  {star}
-                </S.Button>
-              );
-            })}
-          </S.RatingButtons>
-          <S.SubmitButton
-            type="button"
-            onClick={handleClick}
-            value={submit}
-          >
-            Submit
-          </S.SubmitButton>
-        </>
-        :
-        <Contacts value={submit} />
-      }
+      {/*Separate component*/}
+      <S.Text>
+        <p>How did we do?</p>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </S.Text>
+      {/*Separate component*/}
+      <S.RatingButtons>
+        {[...Array(5)].map((star, index) => {
+          star = index += 1;
+          return (
+            <S.Button
+              type='button'
+              disabled={!!rating} // to delete disabled
+              key={index}
+              className={
+                index === rating ? 'rating-button on' : 'rating-button off'
+              }
+              value={star}
+              onClick={(e) => setRating(Number(e.currentTarget.value))}
+            >
+              {star}
+            </S.Button>
+          );
+        })}
+      </S.RatingButtons>
+
+      {/*Separate component*/}
+      <S.SubmitButton type='button' onClick={() => console.log('test')}>
+        Submit
+      </S.SubmitButton>
     </S.HeroWrapper>
   );
 };
